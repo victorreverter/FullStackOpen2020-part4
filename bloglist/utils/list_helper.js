6b9dc7d8d102f622f79fdaf524e62b5ec640702f
victorreverter <victorreverter@gmail.com>
2020-10-30 05:55:06 +0100

    Exercise 4.4

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const total_likes = (blogs) => {
+    const total = blogs.map(blog => blog.likes).reduce((sum, curr) => sum + curr)
+
+    return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+  total_likes
 }
